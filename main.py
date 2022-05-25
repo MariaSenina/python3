446077f1de86fe8e829dbcf8ebaa5c34d0b7376a
Maria Senina <>
2022-05-25 17:33:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Data Types
+
+# Integer -> int
+
+# Float -> float
+
+# Boolean -> bool
+
+# String -> str
+print(type("Hello"))
+
+username = "supercoder"
+password = "supersecret"
+
+some_long_string = '''
+WOW
+O O
+---
+'''
+
+print(some_long_string)
+
+# List -> list
+
+# Tuple -> tuple
+
+# Set -> set
+
+# Dictionary -> dict
+
+
+#Null? -> None
