76c42a3c71aa2fabc0df549817d1f1d5316b4d21
MariaSenina <>
2022-05-25 17:53:29 +0000

    Formatting strings

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ O O
 
 print(some_long_string)
 
+name = input("What is your name? ")
+year = input("What year were you born? ")
+
+age = 2022 - int(year)
+
+print(f'Hi {name}! You are {age} years old.')
+
 # List -> list
 
 # Tuple -> tuple
