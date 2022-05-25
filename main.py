c78539244a78673fd102f823cb0aed97baaf45e3
MariaSenina <>
2022-05-25 18:13:34 +0000

    Variables: basic string manipulation

--- main.py
+++ main.py
@@ -25,7 +25,19 @@ year = input("What year were you born? ")
 
 age = 2022 - int(year)
 
-print(f'Hi {name}! You are {age} years old.')
+print(f'Hi {name}! You are {age} years old. \n')
+
+greeting = "Hello, Python!"
+print(greeting)
+print("This is the 1st letter: " + greeting[0])
+print("This is the 5th letter: " + greeting[4])
+print("This is the range of letters from 1 UNTIL 5: " + greeting[0:4])
+# [start:stop:step_over] -> NOTE: we can skip any part of this expression (defaults - 0:<end>:1)
+print("This is the range of letters with step_over: " + greeting[0:15:2])
+# negative index means "start at the end" (starts at -1)
+print("This is the last character: " + greeting[-1])
+# reverse string -> [::-1]
+print("This is the reversed string: " + greeting[::-1])
 
 # List -> list
 
